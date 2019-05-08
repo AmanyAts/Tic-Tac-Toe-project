ba93b28aeb83f2f4bc3b6f8e0d5a7d4324bb7a1d
AmanyAts <amanyalattass@gmail.com>
2019-05-08 10:38:33 +0300

    third version
    just playing with computer is not working well

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # Tic-Tac-Toe-project
+to build tic tac toe I used html , css and js with jquery library
+As player 1, by defult click on squre to add X
+As player 2, by defult click on squre to add O
+As player 1 if I cliked on O button so 'O' will be added on squre 
+As player 2, 'X' will be added on squre 
+As player , I sould play again if I clicked on play again button
+As player , my score will store it if I press play again so each game I win will counted
+As player , when my turn it 
+--------------------------------------------------------------------------------------
+during develpment process 
+first writing html and css code then js code
+i defined a variable call player to player turn and switching between player 1 & 2 and 
+two variables for character 'X' and 'O' then I used jquery for click event
+I created methode call FindWinner to looking for winner in each time the user add on squre
+to find the winner i used an empty array called playing for storing the elements from div to this array then i used for loop to comparing 
+first if statment for row cases ,second one for column , the last for diagnal
+then if there is a winner the methode will return true otherwise false that is means it's tie
+chooseChar methode this methode will called when X or O buttons clicked is used to change players tokens.
+
--- main.js
+++ main.js
@@ -2,23 +2,78 @@ let playing = [];
 let player = 1;
 let x = "X";
 let o = "O";
-let isWin;
+let isWin=false;
+let isCom=false;
+let score1=0;
+let score2=0;
+
+function withComputer(){
+  $('#player2').text('computer/'+o+'');
+  isCom=true;
+}
+
+function selecting(){
+  //this methode allow the player 1 to select between X and O
+  x=$(event.target).text();
+  if($(event.target).text()!='X'){//swithcing between X and O for the players
+     o='X'
+}else if($(event.target).text()=='X'){
+  o='O'
+}
+//priniting the letter choosed for each player 
+$('#player-char').text('player 1/'+x+'');
+if(!isCom){
+$('#player2-char').text('player 2/'+o+'');
+}else{
+  $('#player2').text('computer/'+o+'');
+}
+
+$('#p1').off('click');
+$('#p2').off('click');
+
+}
 
 function displayChar(event) {
+    
+if(!isCom){
+  
   if (player == 1) {
     //draw x if player is 1
+    //make off to other button
+    $('#com').off("click");
+    $('#p1').off('click');
+    $('#p2').off('click');
     $(event.target).html('<strong class="XandO">' + x + "</strong>");
     $(event.target).off("click"); //off click on the same div
-    let index = $("div").index(this); //get div index to store the char in right postion
-    playing[index] = x;
+    let index = $("div").index(this); //get div index to store the char in right postion in array
+    playing[index] = x; 
     isWin = FindWinner(event);
+    $('#player2').addClass('turn');
+    $('#player1').removeClass('turn');
+    if(isWin) {//if player 1 win
+      console.log('ttttttttttt')
+      
+      score1++;
+      console.log(score1);
+     $('#socre1').text('='+score1)
+    }
+    
   } else if (player == 2) {
     //draw o if player is 1
+    
     $(event.target).html('<strong class="XandO">' + o + "</strong>");
     $(event.target).off("click");
     let index = $("div").index(this);
     playing[index] = o;
     isWin = FindWinner(event);
+    $('#player2').removeClass("turn");
+    $('#player1').addClass('turn');
+    if(isWin){
+      score2++;
+      $('#socre2').text('='+score2)
+      console.log(score2);
+    } 
+    
   }
 
   if (!isWin) {
@@ -27,85 +82,69 @@ function displayChar(event) {
         //to make sure is switching between 1 and 2
         player = 1;
     }
-    $("#y").text("player " + player + "'s turn");
     tie(isWin);
   }
-  //     if(playing[0]==playing[0+1]&&playing[0+1]==playing[0+2]&&playing[0]!=null){
-  // console.log('wwwwwwwwwwwwwwwwwwwwwwwwwwwww')
-  //     }
-  // if($('#1').text() == $('#2').text() && $('#2').text()==$('#3').text()){
-  //     console.log('firstCase')
-  //     console.log($(event.target).text()+' isWinner')
-  // }
-  // else if($('#4').text() == $('#5').text() && $('#5').text()==$('#6').text()){
-  //     console.log('secCase')
-  //     console.log($(event.target).text()+' isWinner')
-  // }
-  // else if($('#7').text() == $('#8').text() && $('#8').text()==$('#9').text()){
-  //     console.log($(event.target).text()+' isWinner')
-  // }else if($('#1').text() == $('#4').text() && $('#4').text()==$('#7').text()){
-  //     console.log($(event.target).text()+' isWinner')
-  // }else if($('#2').text() == $('#5').text() && $('#5').text()==$('#8').text()){
-  //     console.log($(event.target).text()+' isWinner')
-  // }else if($('#3').text() == $('#6').text() && $('#6').text()==$('#9').text()){
-  //     console.log($(event.target).text()+' isWinner')
-  // }else if($('#1').text() == $('#5').text() && $('#5').text()==$('#9').text()){
-  //     console.log($(event.target).text()+' isWinner')
-  // }else if($('#3').text() == $('#5').text() && $('#5').text()==$('#7').text()){
-  //     console.log($(event.target).text()+' isWinner')
-  // }
+}else{//computer playing
+  
+  
+  $(event.target).html('<strong class="XandO">' + x + "</strong>");
+  let index = $("div").index(this); //get div index to store the char in right postion
+  playing[index] = x;
+  let random;
+  do{
+    random= Math.floor(Math.random() * 10);
+  }while(document.querySelectorAll(".box")[random].innerHTML!="")
+  console.log('raa '+random);
+  document.querySelectorAll(".box")[random].innerHTML='<strong class="XandO">' + o + "</strong>";
+
+  playing[random] =o;
+  console.log(playing);
+  isWin = FindWinner(event);
+}
+
 }
 
 function FindWinner(event) {
   //this methode to looking for winner
   console.log("playerrr  " + player);
 
+
   for (let i = 0; i < playing.length; i++) {
       console.log("hi")
     if (i == 0 || i == 3 || i == 6) {
       //row cases
       console.log("ok")
-      if (
-        playing[i] == playing[i + 1] &&
-        playing[i] == playing[i + 2] &&
-        playing[i] != null
-      ) {
+      if ( playing[i] == playing[i + 1] &&playing[i] == playing[i + 2] &&playing[i] != null) {
+        
           console.log("winner")
-        $("#y").html("<h3>player " + player + " is winner</h3>");
-        $("div").off("click");
-        return true;
+          swal("player " + player + " is winner");
+          // $('#'+i).toggleClass('win');
+          // $('#'+(i+1)).css('color','gold');
+          // $('#'+(i+2)).css('color','gold');
+          $("div").off("click");
+           return true;
       }
     }
     if (i == 0 || i == 1 || i == 2) {
       //column cases
-      if (
-        playing[i] == playing[i + 3] &&
-        playing[i] == playing[i + 6] &&
-        playing[i] != null
-      ) {
-        $("#y").html("<h3>player " + player + " is winner</h3>");
+      if (playing[i] == playing[i + 3] &&playing[i] == playing[i + 6] &&playing[i] != null) {
+        swal("player " + player + " is winner");
         $("div").off("click");
         return true;
       }
     }
   }
-  if (
-    playing[0] == playing[4] &&
-    playing[0] == playing[8] &&
-    playing[0] != null
-  ) {
+  if (playing[0] == playing[4] &&playing[0] == playing[8] &&playing[0] != null) {
     //diagnols
-    $("#y").html("<h3>player " + player + " is winner</h3>");
+    swal("player " + player + " is winner");
+
 
     $("div").off("click");
     return true;
-  } else if (
-    playing[2] == playing[4] &&
-    playing[2] == playing[6] &&
-    playing[2] != null
-  ) {
+  } else if (playing[2] == playing[4] &&playing[2] == playing[6] &&playing[2] != null) {
     //diagnols
-    $("#y").html("<h3>player " + player + " is winner</h3>");
+    swal("player " + player + " is winner");
+
     $("div").off("click");
     return true;
   }
@@ -122,14 +161,30 @@ function tie(isWin) {
     //to make sure all boxes is full
     if (isWin === false) {
       //Findwinner will return false if there is no winner
-      $("#y").html("<h3>Tie</h3>");
+      swal("No winner");
+
     }
   }
 }
 
 function restGame() {
-  location.reload();
+  $("#y").text("");
+  $('.box').text('');
+  playing=[];
+  player=1;
+  $('#player2').removeClass("turn");
+  $('#player1').addClass('turn');
+  console.log($('.box').text())
+  console.log(playing)
+  $(".box").on("click", displayChar);
+  $('#p1').on('click',selecting);
+$('#p2').on('click',selecting);
+  // location.reload();
 }
-
-$("button").on("click", restGame);
+$('#p1').on('click', selecting);
+$('#p2').on('click', selecting);
+$("#rest").on("click", restGame);
 $(".box").on("click", displayChar);
+$('#com').on("click", withComputer);
+
+
